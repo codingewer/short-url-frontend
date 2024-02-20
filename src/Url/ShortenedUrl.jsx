dfd100a1ea020f86e2a705fde79fad201e4844ce
Codingewer <yatli5645@gmail.com>
2024-02-20 19:30:50 +0300

    dgfg

--- src/Url/ShortenedUrl.jsx
+++ src/Url/ShortenedUrl.jsx
@@ -2,23 +2,14 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import AdSense from "react-adsense";
-import "./Ads.css"
+import "./Ads.css";
+import AdsComponent from "./AdsComponent";
 
 function ShortenedUrl() {
   const { shortenedUrl } = useParams();
   const { adIndex } = useParams();
   const [url, setUrl] = useState();
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:8180/url/get/" + shortenedUrl)
-      .then(function (response) {
-        setUrl(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
   const currentURL = window.location.href;
   const domain = currentURL.split("r/" + adIndex)[0];
   console.log(domain);
@@ -33,17 +24,9 @@ function ShortenedUrl() {
   console.log(adIndex);
   return (
     <div className="ads-container">
-      <div className="ad-content" >
-        <AdSense.Google
-          style={{ display: "block", backgroundColor: "green" }}
-          client="pub-4301229156748291" // AdSense yayıncı kimliği
-          slot="3634852612"
-          format="auto"
-          responsive="true"
-          onAdLoaded={() => console.log("Reklam yüklendi")}
-          onAdFailed={() => console.log("Reklam yüklenemedi")}
-        />
+      <div className="ad-content">
       </div>
+      <AdsComponent dataAdSlot = "3634852612"/>
       <button onClick={handleSkip}>Reklamı Göster</button>
     </div>
   );
